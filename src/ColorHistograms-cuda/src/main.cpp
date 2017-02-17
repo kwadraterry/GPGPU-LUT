ca5b3c39de3ad47f712b00b24d9fd593faa26a2e
Maria Glukhova <siamezzze@gmail.com>
2017-02-18 01:57:27 +0200

    No need for writing function.

--- src/ColorHistograms-cuda/src/main.cpp
+++ src/ColorHistograms-cuda/src/main.cpp
@@ -79,50 +79,6 @@ void read_png_file(char *filename) {
   fclose(fp);
 }
 
-void write_png_file(char *filename) {
-  int y;
-
-  FILE *fp = fopen(filename, "wb");
-  if(!fp) abort();
-
-  png_structp png = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
-  if (!png) abort();
-
-  png_infop info = png_create_info_struct(png);
-  if (!info) abort();
-
-  if (setjmp(png_jmpbuf(png))) abort();
-
-  png_init_io(png, fp);
-
-  // Output is 8bit depth, RGBA format.
-  png_set_IHDR(
-    png,
-    info,
-    width, height,
-    8,
-    PNG_COLOR_TYPE_RGBA,
-    PNG_INTERLACE_NONE,
-    PNG_COMPRESSION_TYPE_DEFAULT,
-    PNG_FILTER_TYPE_DEFAULT
-  );
-  png_write_info(png, info);
-
-  // To remove the alpha channel for PNG_COLOR_TYPE_RGB format,
-  // Use png_set_filler().
-  // png_set_filler(png, 0, PNG_FILLER_AFTER);
-
-  png_write_image(png, row_pointers);
-  png_write_end(png, NULL);
-
-  for(int y = 0; y < height; y++) {
-    free(row_pointers[y]);
-  }
-  free(row_pointers);
-
-  fclose(fp);
-}
-
 void process_png_file() {
   for(int y = 0; y < height; y++) {
     png_bytep row = row_pointers[y];
@@ -135,11 +91,10 @@ void process_png_file() {
 }
 
 int main(int argc, char *argv[]) {
-  if(argc != 3) abort();
+  if(argc != 2) abort();
 
   read_png_file(argv[1]);
   process_png_file();
-  write_png_file(argv[2]);
 
   return 0;
 }
