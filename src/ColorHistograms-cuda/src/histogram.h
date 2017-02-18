81e42e5a43b058ec0df76c078752b60765bfbdcb
Maria Glukhova <siamezzze@gmail.com>
2017-02-18 05:14:12 +0200

    GPU histogram computing.
    Add GPU histogram computing using global atomics (uses global memory
    to store local histogram, then accumulates them).
    Based on: https://devblogs.nvidia.com/parallelforall/gpu-pro-tip-fast-histograms-using-shared-atomics-maxwell/
    
    Closes: #2

---
+++ src/ColorHistograms-cuda/src/histogram.h
@@ -0,0 +1,72 @@
+/******************************************************************************
+ * Copyright (c) 2011-2016, NVIDIA CORPORATION.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the NVIDIA CORPORATION nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL NVIDIA CORPORATION BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ ******************************************************************************/
+
+#ifndef HISTOGRAM_H
+#define HISTOGRAM_H
+
+#define NUM_BINS 16
+#define ACTIVE_CHANNELS 4
+#define NUM_PARTS 64
+#define PixelType uchar4
+
+__device__ __forceinline__ void DecodePixel(uchar4 pixel, unsigned int (&bins)[ACTIVE_CHANNELS]);
+
+__global__ void histogram_smem_atomics(
+	const PixelType *in,
+	int width,
+	int height,
+	unsigned int *out);
+
+__global__ void histogram_smem_accum(
+	const unsigned int *in,
+	int n,
+	unsigned int *out);
+
+void run_smem_atomics(
+	PixelType *d_image,
+	int width,
+	int height,
+	unsigned int *d_hist);
+
+__global__ void histogram_gmem_atomics(
+	const PixelType *in,
+	int width,
+	int height,
+	unsigned int *out);
+
+__global__ void histogram_gmem_accum(
+	const unsigned int *in,
+	int n,
+	unsigned int *out);
+
+void run_gmem_atomics(
+	PixelType *d_image,
+	int width,
+	int height,
+	unsigned int *d_hist);
+
+#endif
--- src/ColorHistograms-cuda/src/main.cpp
+++ src/ColorHistograms-cuda/src/main.cpp
@@ -1,7 +1,7 @@
 
 // CUDA utilities and system includes
 #include <cuda_runtime.h>
-#include <cuda_gl_interop.h>
+#include "device_launch_parameters.h"
 
 // Helper functions
 #include <helper_functions.h>  // CUDA SDK Helper functions
@@ -13,10 +13,33 @@
 #include <png.h>
 
 #include "imageio.h"
+#include "histogram.h"
+
+// Debug: Print pixel values.
+void probe_image(PixelType* image, png_infop info) {
+	PixelType pixel;
+	for (uint y = 0; y < info->height; y+=10){
+		for (uint x = 0; x < info->width; x+=10){
+			pixel = image[y * info->width + x];
+			unsigned char* samples = reinterpret_cast<unsigned char*>(&pixel);
+
+			printf("[");
+			#pragma unroll
+			for (int CHANNEL = 0; CHANNEL < ACTIVE_CHANNELS; ++CHANNEL) {
+				printf("%4u", (unsigned int) (samples[CHANNEL]));
+			}
+			printf("]");
+		}
+		printf("\n");
+	}
+}
 
 
 int main (int argc, char* argv[]) {
-	uchar4 *host_in;
+	PixelType* h_pixels;
+    PixelType* d_pixels;
+    unsigned int* d_hist;
+    unsigned int* h_hist;
 
 	png_infop info;
 
@@ -25,13 +48,43 @@ int main (int argc, char* argv[]) {
 		return -1;
 	}
 
-	if(read_png(argv[1], &info, &host_in) == PNG_FAILURE) {
+	if(read_png(argv[1], &info, &h_pixels) == PNG_FAILURE) {
 		printf("Error reading file (%s).\n", argv[1]);
 		return -1;
 	}
 
 	size_t number_of_bytes = sizeof(uchar4) * info->width * info->height;
 
-	printf("Image has height %lu and width %lu\n", info->height, info->width);
+	printf("Image has height %lu and width %lu\n", info->width, info->width);
+
+
+    // Copy data to GPU
+    printf("...allocating GPU memory and copying input data\n\n");
+    checkCudaErrors(cudaMalloc((void **)&d_pixels, number_of_bytes));
+    checkCudaErrors(cudaMalloc((void **)&d_hist, ACTIVE_CHANNELS * NUM_BINS * sizeof(uint)));
+    checkCudaErrors(cudaMemcpy(d_pixels, h_pixels, number_of_bytes, cudaMemcpyHostToDevice));
+
+    // Run histogram computing
+    run_gmem_atomics(d_pixels, info->width, info->width, d_hist);
+    printf("Done.\n");
+
+    // Copy result back to CPU
+    h_hist = (uint* )malloc(ACTIVE_CHANNELS * NUM_BINS * sizeof(uint));
+    checkCudaErrors(cudaMemcpy(h_hist, d_hist, ACTIVE_CHANNELS * NUM_BINS * sizeof(uint), cudaMemcpyDeviceToHost));
+
+    // Print results.
+    printf("Histogram:\n");
+    for (uint ch = 0; ch < ACTIVE_CHANNELS; ch++) {
+    	printf("Channel %u:\n", ch + 1);
+    	for (uint bin = 0; bin < NUM_BINS; bin++) {
+        	printf("%5u ", h_hist[ch * NUM_BINS + bin]);
+    	}
+    	printf("\n");
+    }
+
+    checkCudaErrors(cudaFree(d_pixels));
+    checkCudaErrors(cudaFree(d_hist));
+    free(h_pixels);
+    free(h_hist);
 	return 0;
 }
